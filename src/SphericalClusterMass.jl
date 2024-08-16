c353df1e56b95ed6096dd79767ce456e049d1aa9
tmistele <29761456+tmistele@users.noreply.github.com>
2024-08-16 19:03:37 -0400

    Add option to interpolate and calculate I/J integrals in log-R space
    May be slightly better for log bins
    
    Also remove leftover ExtrapolateFlat

--- src/SphericalClusterMass.jl
+++ src/SphericalClusterMass.jl
@@ -3,6 +3,6 @@ module SphericalClusterMass
 include("cluster_deproject_notebook.jl")
 
 export calculate_gobs_fconst, calculate_gobs_fgeneral, calculate_gobs_and_covariance_in_bins
-export ExtrapolateZero, ExtrapolatePowerDecay
+export ExtrapolatePowerDecay, InterpolateR, InterpolateLnR
 
 end # module SphericalClusterMass
