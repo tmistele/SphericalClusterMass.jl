20db384c1943ce302fd2986b664737128b960e7f
Tobias Mistele <tobias.mistele@case.edu>
2024-10-08 19:00:18 -0400

    Initial support for (simple) miscentering correction

--- src/SphericalClusterMass.jl
+++ src/SphericalClusterMass.jl
@@ -3,6 +3,6 @@ module SphericalClusterMass
 include("cluster_deproject_notebook.jl")
 
 export calculate_gobs_fconst, calculate_gobs_fgeneral, calculate_gobs_and_covariance_in_bins
-export ExtrapolatePowerDecay, InterpolateR, InterpolateLnR
+export ExtrapolatePowerDecay, InterpolateR, InterpolateLnR, MiscenterCorrectNone, MiscenterCorrectSmallRmc
 
 end # module SphericalClusterMass
